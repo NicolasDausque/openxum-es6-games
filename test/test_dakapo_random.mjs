21e57ede0674fcab70734df1ead2fdc392e5ae7c
Nicolas Dausque <nicolas.dausque@outlook.fr>
2018-06-12 13:50:17 +0200

    engine methode secondaire

--- test/test_dakapo_random.mjs
+++ test/test_dakapo_random.mjs
@@ -2,11 +2,9 @@ import lib from '../lib/openxum-core/openxum';
 
 let e = new lib.OpenXum.Dakapo.Engine();
 
-
 console.log("Arthur");
-let move=(lib.OpenXum.Dakapo.Color.GREEN, 1,1);
+let move=(lib.OpenXum.Dakapo.Color.GREEN, 4,5);
 e.move(move);
 
 console.log(move);
 
-console.log("Arthur");
