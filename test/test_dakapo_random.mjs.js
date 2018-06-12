19dfccf839ae71592a262022ee5552a9653603ce
Nicolas Dausque <nicolas.dausque@outlook.fr>
2018-06-12 10:58:16 +0200

    creation du repo Dakapo

---
+++ test/test_dakapo_random.mjs.js
@@ -0,0 +1,12 @@
+import lib from '../lib/openxum-core/openxum';
+
+let e = new lib.OpenXum.Dakapo.Engine();
+
+
+console.log("Arthur");
+let move=(lib.OpenXum.Dakapo.Color.GREEN, 1,1);
+e.move(move);
+
+console.log(move);
+
+console.log("Arthur");
