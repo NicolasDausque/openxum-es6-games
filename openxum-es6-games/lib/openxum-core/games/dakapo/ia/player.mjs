eb9d08e35181af5198173709fe36756c986835bc
ArthurDetant <arthur.detant1996@gmail.com>
2018-06-20 15:53:50 +0200

    Fin de séance, IA problemes

--- openxum-es6-games/lib/openxum-core/games/dakapo/ia/player.mjs
+++ openxum-es6-games/lib/openxum-core/games/dakapo/ia/player.mjs
@@ -1,27 +1,26 @@
 "use strict";
-import Color from '../color.mjs';
+import Move from '../move.mjs';
 
 class Player {
   constructor(c, e, depth) {
     this._color = c;
     this._engine = e;
     this._depth = depth;
-    this._cpt = 0;
   }
 
   move() {
     let possibleMoves = this._engine.get_possible_move_list();
     let chosenMove = possibleMoves[Math.floor(Math.random() * (possibleMoves.length - 1))];
     let clone = this._engine.clone();
-    let score = -99999;
+    let score = -1000000000;
+    let score_tempo;
 
-    this._cpt = this._cpt + possibleMoves.length - 1;
     for (let i = 0; i < possibleMoves.length - 1; i++) {
       if (this._engine._carre(possibleMoves[i])) {
         chosenMove = possibleMoves[i];
         break;
       }
-      let score_tempo = this._get_score(possibleMoves[i], clone, this._depth, -1);
+      score_tempo = this._get_score(possibleMoves[i], clone, this._depth - 1, -1, score);
       if (score_tempo > score) {
         score = score_tempo;
         chosenMove = possibleMoves[i];
@@ -30,48 +29,38 @@ class Player {
     return chosenMove;
   }
 
-
-  _get_score(move, aclone, depth, turn) {
+  _get_score(move, aclone, depth, turn, score_parent) {
+    let score = 0;
+    let list;
 
     aclone.move(move);
-    let list = aclone.get_possible_move_list();
-
     if (depth === 0) {
-      this._cpt = this._cpt + 1;
-      if (turn > 0) {
-        return this._evaluer_max(move, aclone);
-      }
-      else if (turn < 0) {
-        return this._evaluer_min(move, aclone);
-      }
+      return this._evaluer(move, aclone, turn);
     }
-
-    let score = 0;
-
+    if (aclone._carre(move)) {
+      return turn * 100 * (10 ^ depth);
+    }
+    list = aclone.get_possible_move_list();
     for (let i = 0; i < list.length - 1; i++) {
-      let score_tempo = this._get_score(list[i], aclone, depth - 1, -turn);
-
+      let score_tempo = this._get_score(list[i], aclone, depth - 1, -turn, score);
       if (turn * score_tempo >= turn * score) {
         score = score_tempo;
+      }
+      if (score * turn > score_parent * turn) {
+        break;
       }
     }
     return score;
   }
 
-  _evaluer_min(move, clone) {
+  _evaluer(move, clone, turn, depth) {
+    let score = 0;
     if (clone._carre(move)) {
-      return -1000;
+      score = turn * 100 * (10 ^ depth);
     }
-    return 0;
+    score = score + turn * (7 - Math.abs((move._abs + move._ord) - 7));
+    return score;
   }
-
-  _evaluer_max(move, clone) {
-    if (clone._carre(move)) {
-      return 1000;
-    }
-    return 0;
-  }
-
 
 }
 
